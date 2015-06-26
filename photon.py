c6bbb81e31aa04455f8f83e199099a59c199d4a7
uber2 <india185@riseup.net>
2015-06-26 20:47:29 +0200

    Dialog to choose folders

--- photon.py
+++ photon.py
@@ -4,8 +4,8 @@ import os
 import json
 import logging
 import pprint
-#import shutil
 import re
+import tkFileDialog as tk
 
 logging.basicConfig(level=logging.WARNING,format='%(asctime)s %(name)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename ="trace.log")
 logger = logging.getLogger("photon")
@@ -32,28 +32,36 @@ def bringordertochaos(base, target):
 				"04":"04_April","05":"05_May","06":"06_June",
 				"07":"07_July","08":"08_August","09":"09_September",
 				"10":"10_Oktober","11":"11_November","12":"12_December"}
+
+	permittedextensions = ["png","jpg","mov","jpeg","avi","bmp","tiff"]
+
 	for root, dirs, files in os.walk(target):
 		for name in files:
-			#ext = name[len(name)-4:len(name)]
-			#if ext == ".JPG" or ext == ".jpg":
-			try:
-				datetime = getoriginaltimeanddate(root + "\\" + name)
-			except:
-				oldpath = root + "\\" + name
-				newpath = base + "\\" + "Photon" + "\\" + "_unassigned" + "\\"
-				if not os.path.exists(newpath): os.makedirs(newpath)
-				os.rename(oldpath,newpath + name)
-				next
-			try:
-				oldpath = root + "\\" + name
-				newpath = base + "\\" + "Photon" + "\\" + datetime[0:4] + "\\" + month[datetime[4:6]]
-				if not os.path.exists(newpath): os.makedirs(newpath)
-				os.rename(oldpath,newpath + "\\" + "IMG_" + datetime + ".JPG" )
-			except:
-				logger.warning("file: %s, error: %s",name,sys.exc_info()[0:2])
-				continue
+			# MISSING: IMPLEMENT SAFTEY SWITCH; ONLY HANDLE JPEG;JPG;TIFF;MOV;AVI;BMP;
+			ext = name[len(name)-4:len(name)]
+			if ext[0] == "." :ext = ext[1:]
+			if ext.lower() in permittedextensions:
+				try:
+					datetime = getoriginaltimeanddate(root + "\\" + name)
+				except:
+					oldpath = root + "\\" + name
+					newpath = base + "\\" + "Photon" + "\\" + "_unassigned" + "\\"
+					if not os.path.exists(newpath): os.makedirs(newpath)
+					os.rename(oldpath,newpath + name)
+					next
+				try:
+					oldpath = root + "\\" + name
+					newpath = base + "\\" + "Photon" + "\\" + datetime[0:4] + "\\" + month[datetime[4:6]]
+					if not os.path.exists(newpath): os.makedirs(newpath)
+					# MISSING: FIND OUT ORGINIAL EXTENSION (COULD BE TIFF OR JPEG OR JPG)
+					os.rename(oldpath,newpath + "\\" + "IMG_" + datetime + ".JPG" )
+				except:
+					logger.warning("file: %s, error: %s",name,sys.exc_info()[0:2])
+					continue
 
 if __name__ == "__main__":
-	base = sys.argv[1]
-	target = sys.argv[2]
+	base = tk.askdirectory(title="base")
+	target = tk.askdirectory(title="target")
+#	base = sys.argv[1]
+#	target = sys.argv[2]
 	bringordertochaos(base,target)
